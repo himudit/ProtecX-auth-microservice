a8f5aa702fc215357a0e977acb2fdbf55befa9a1
himudit <gargmudit662@gmail.com>
2026-01-25 16:43:55 +0530

    removing unnecessary files

--- internal/utils/password.go
+++ internal/utils/password.go
@@ -34,56 +34,6 @@ func HashPassword(password string) (string, error) {
 	return fmt.Sprintf("%s.%s", saltStr, hashStr), nil
 }
 
-// VerifyPassword checks if the given password matches the stored hash
-// func VerifyPassword(storedHash, password string) (bool, error) {
-// 	// Split the stored hash into salt and hash
-// 	// parts := []byte(storedHash)
-// 	split := 0
-// 	for i := 0; i < len(storedHash); i++ {
-// 		if storedHash[i] == '.' {
-// 			split = i
-// 			break
-// 		}
-// 	}
-// 	if split == 0 {
-// 		return false, fmt.Errorf("invalid stored hash format")
-// 	}
-
-// 	saltStr := storedHash[:split]
-// 	hashStr := storedHash[split+1:]
-
-// 	salt, err := base64.RawStdEncoding.DecodeString(saltStr)
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	expectedHash, err := base64.RawStdEncoding.DecodeString(hashStr)
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	// Argon2id parameters must match those used in HashPassword
-// 	timeCost := uint32(1)
-// 	memory := uint32(64 * 1024)
-// 	threads := uint8(4)
-// 	keyLen := uint32(32)
-
-// 	computedHash := argon2.IDKey([]byte(password), salt, timeCost, memory, threads, keyLen)
-
-// 	if len(computedHash) != len(expectedHash) {
-// 		return false, nil
-// 	}
-
-// 	// Constant-time comparison
-// 	diff := byte(0)
-// 	for i := 0; i < len(expectedHash); i++ {
-// 		diff |= computedHash[i] ^ expectedHash[i]
-// 	}
-
-// 	return diff == 0, nil
-// }
-
-
 func VerifyPassword(storedHash, password string) (bool, error) {
 	// Split stored hash into salt and hash
 	parts := strings.SplitN(storedHash, ".", 2)
