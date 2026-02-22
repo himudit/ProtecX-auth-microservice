74115714676572b7a684c8d2efafd8d0ae6ceb2e
himudit <gargmudit662@gmail.com>
2026-02-22 23:12:59 +0530

    restring select fields

--- internal/domain/project_user.go
+++ internal/domain/project_user.go
@@ -3,17 +3,15 @@ package domain
 import "time"
 
 type ProjectUser struct {
-	ID         string
-	ProjectID  string
-	ProviderID string
-
-	Name         string
-	Email        string
-	PasswordHash string
-	Role         ProjectRole
-	IsVerified   bool
-	TokenVersion int
-
-	CreatedAt   time.Time
-	LastLoginAt *time.Time
+	ID           string      `json:"id"`
+	ProjectID    string      `json:"-"`
+	ProviderID   string      `json:"-"`
+	Name         string      `json:"name"`
+	Email        string      `json:"email"`
+	PasswordHash string      `json:"-"`
+	Role         ProjectRole `json:"role"`
+	IsVerified   bool        `json:"isVerified"`
+	TokenVersion int         `json:"-"`
+	CreatedAt    time.Time   `json:"-"`
+	LastLoginAt  *time.Time  `json:"lastLoginAt"`
 }
